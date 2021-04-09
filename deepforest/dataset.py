711c3c57446161b6b7d4554e99ac0662ed6029b8
Dwaipayan Munshi <53687927+dwaipayan05@users.noreply.github.com>
2021-04-09 23:27:23 +0530

    Update dataset.py

--- deepforest/dataset.py
+++ deepforest/dataset.py
@@ -12,14 +12,12 @@ labels (Int64Tensor[N]): the class label for each ground-truth box
 https://colab.research.google.com/github/benihime91/pytorch_retinanet/blob/master/demo.ipynb#scrollTo=0zNGhr6D7xGN
 
 """
-import glob
 import os
-import csv
 import pandas as pd
 from skimage import io
 from torch.utils.data import Dataset
 from deepforest import transforms as T
-from deepforest import utilities
+from deepforest.utilities import check_image
 
 
 def get_transform(augment):
@@ -30,48 +28,36 @@ def get_transform(augment):
     return T.Compose(transforms)
 
 
-# idx_to_label = {"Tree": 0}
-
 
 class TreeDataset(Dataset):
 
-    def __init__(self, csv_file, root_dir, transforms):
+    def __init__(self, csv_file, root_dir, transforms, label_dict = {"Tree": 0}):
         """
         Args:
             csv_file (string): Path to a single csv file with annotations.
             root_dir (string): Directory with all the images.
             transform (callable, optional): Optional transform to be applied
                 on a sample.
+            label_dict: a dictionary where keys are labels from the csv column and values are numeric labels "Tree" -> 0
         """
         self.annotations = pd.read_csv(csv_file)
         self.root_dir = root_dir
         self.transform = transforms
         self.image_names = self.annotations.image_path.unique()
-        self.read_classes()
-        self.csvfile = csv_file
+        self.label_dict = label_dict
 
     def __len__(self):
         return len(self.image_names)
 
-    def read_classes(self):
-        """Read class file in case of multi-class training.
-        If no file has been created, DeepForest assume there is 1 class,
-        Tree
-        
-        """        
-        self.labels = {}
-        try:
-            with open(self.csvfile, 'r') as file:
-                self.classes = _read_classes(csv.reader(file, delimiter=','))
-            for key, value in self.classes.items():
-                self.labels[value] = key
-        except:
-            self.labels[0] = "Tree"
-    
     def __getitem__(self, idx):
         img_name = os.path.join(self.root_dir, self.image_names[idx])
         image = io.imread(img_name)
         image = image / 255
+        
+        try:
+            check_image(image)
+        except Exception as e:
+            raise Exception("dataloader failed with exception for image: {}",format(img_name))
 
         # select annotations
         image_annotations = self.annotations[self.annotations.image_path ==
@@ -80,9 +66,9 @@ class TreeDataset(Dataset):
         targets["boxes"] = image_annotations[["xmin", "ymin", "xmax",
                                               "ymax"]].values.astype(float)
 
-        # Labels need to be encoded? 0 or 1 indexed?, ALL tree for the moment.
+        # Labels need to be encoded
         targets["labels"] = image_annotations.label.apply(
-            lambda x: self.labels[x]).values.astype(int)
+            lambda x: self.label_dict[x]).values.astype(int)
 
         if self.transform:
             image, targets = self.transform(image, targets)
